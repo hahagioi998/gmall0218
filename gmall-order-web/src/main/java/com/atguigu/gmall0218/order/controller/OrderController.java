f5aec6caf07bf0c7d7be4a18b4df6ced89bcd6a6
琪玉 <315199533@qq.com>
2019-08-14 11:02:22 +0800

    电商项目完结测试提交单个类

--- gmall-order-web/src/main/java/com/atguigu/gmall0218/order/controller/OrderController.java
+++ gmall-order-web/src/main/java/com/atguigu/gmall0218/order/controller/OrderController.java
@@ -1,36 +1,174 @@
 package com.atguigu.gmall0218.order.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
+import com.atguigu.gmall0218.bean.CartInfo;
+import com.atguigu.gmall0218.bean.OrderDetail;
+import com.atguigu.gmall0218.bean.OrderInfo;
 import com.atguigu.gmall0218.bean.UserAddress;
+import com.atguigu.gmall0218.bean.enums.OrderStatus;
+import com.atguigu.gmall0218.bean.enums.ProcessStatus;
+import com.atguigu.gmall0218.handler.LoginRequire;
+import com.atguigu.gmall0218.service.CartService;
+import com.atguigu.gmall0218.service.OrderService;
 import com.atguigu.gmall0218.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author qiyu
  * @create 2019-07-23 13:03
- * @Description:TODO(这里用一句话来描述这个类的作用)
+ * @Description:
  */
-@RestController
+@Controller
 public class OrderController {
 
 
     @Reference
     private UserService userService;
 
-    @RequestMapping("trade")
-    @ResponseBody // 第一个返回json 字符串，fastJson.jar 第二直接将数据显示到页面！
-    public List<UserAddress> trade(String userId){
+    @Reference
+    private CartService cartService;
+
+    @Reference
+    private OrderService orderService;
+
+    /**
+     *生成结算页面数据
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "trade",method = RequestMethod.GET)
+    @LoginRequire
+    public String tradeInit(HttpServletRequest request){
+
+        String userId = (String) request.getAttribute("userId");
 
 
-        return userService.getUserAddressList(userId);
+
+        // 获取TradeCode号
+        String tradeNo = orderService.getTradeNo(userId);
+        request.setAttribute("tradeCode",tradeNo);
+
+        // 得到选中的购物车列表
+        List<CartInfo> cartCheckedList = cartService.getCartCheckedList(userId);
+
+
+        // 收货人地址
+        List<UserAddress> userAddressList = userService.getUserAddressList(userId);
+        request.setAttribute("userAddressList",userAddressList);
+
+
+        // 订单信息集合
+        List<OrderDetail> orderDetailList=new ArrayList<>(cartCheckedList.size());
+        for (CartInfo cartInfo : cartCheckedList) {
+
+            OrderDetail orderDetail = new OrderDetail();
+            orderDetail.setSkuId(cartInfo.getSkuId());
+            orderDetail.setSkuName(cartInfo.getSkuName());
+            orderDetail.setImgUrl(cartInfo.getImgUrl());
+            orderDetail.setSkuNum(cartInfo.getSkuNum());
+            orderDetail.setOrderPrice(cartInfo.getCartPrice());
+            orderDetailList.add(orderDetail);
+
+
+        }
+
+
+        OrderInfo orderInfo=new OrderInfo();
+        orderInfo.setOrderDetailList(orderDetailList);
+        orderInfo.sumTotalAmount();
+
+        request.setAttribute("totalAmount",orderInfo.getTotalAmount());
+        request.setAttribute("orderDetailList",orderDetailList);
+
+
+        return  "trade";
     }
 
+    /**
+     * 下订单的时候添加到数据库中
+     * @param orderInfo
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "submitOrder",method = RequestMethod.POST)
+    @LoginRequire
+    public String submitOrder(OrderInfo orderInfo,HttpServletRequest request){
+
+        String userId = (String) request.getAttribute("userId");
+        //
+        // 检查tradeCode防止重复提交订单
+        String tradeNo = request.getParameter("tradeNo");
+        boolean flag = orderService.checkTradeCode(userId, tradeNo);
+        if(!flag){
+            request.setAttribute("errMsg","该页面已失效，请重新结算!");
+            return "tradeFail";
+        }
+
+
+        // 初始化参数
+        orderInfo.setOrderStatus(OrderStatus.UNPAID);
+        orderInfo.setProcessStatus(ProcessStatus.UNPAID);
+        orderInfo.sumTotalAmount();
+        orderInfo.setUserId(userId);
+        //调用第三方工程
+        //从校验库存
+        List<OrderDetail> orderDetailList = orderInfo.getOrderDetailList();
+        for (OrderDetail orderDetail : orderDetailList) {
+            boolean result = orderService.checkStock(orderDetail.getSkuId(), orderDetail.getSkuNum());
+            if(!result){
+                request.setAttribute("errMsg",orderDetail.getSkuName()+":商品库存不足，请重新下单！");
+                return "tradeFail";
+            }
+        }
+
+
+        // 保存
+        String orderId = orderService.saveOrder(orderInfo);
+        // 删除tradeNo
+        orderService.delTradeCode(userId);
+
+        // 重定向
+        return "redirect://payment.gmall.com/index?orderId="+orderId;
+
+    }
+
+
+    /**
+     * 拆单接口
+     * @param request
+     * @return
+     */
+    @RequestMapping("orderSplit")
+    @ResponseBody
+    public String orderSplit(HttpServletRequest request){
+        String orderId = request.getParameter("orderId");
+        String wareSkuMap = request.getParameter("wareSkuMap");
+
+        // 返回的是子订单集合
+        List<OrderInfo> orderInfoList = orderService.orderSplit(orderId,wareSkuMap);
+
+        // 创建一个集合 来存储map
+        List<Map> wareMapList=new ArrayList<>();
+
+        for (OrderInfo orderInfo : orderInfoList) {
+            Map map = orderService.initWareOrder(orderInfo);
+            wareMapList.add(map);
+        }
+
+
+        return JSON.toJSONString(wareMapList);
+    }
 
 
 }
